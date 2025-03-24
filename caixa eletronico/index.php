2979f8dc961215c8a6e9b8a6b6feda0b69260fa4
alisson <159741483+jubileuabravanel@users.noreply.github.com>
2025-03-24 16:57:54 -0300

    Add files via upload

---
+++ caixa eletronico/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$notas = [100, 50, 20, 10, 5 , 2, 1];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $valor = $_POST["valor"];
+    $saque = $_POST[];
+    $sobra
+
+    foreach ($notas as $nota) {
+        $quantidade
+            echo " <img src='' "
+    }
+
+
+}
+?>
